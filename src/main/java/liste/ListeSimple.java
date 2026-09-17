842be04043744c0fd1066416b30bc1c1fd2f5f26
MdialloC19 <mdiallo3211@gmail.com>
2026-09-17 18:45:20 +0200

    ci: add maven and sonar caching

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -21,6 +21,7 @@ public class ListeSimple {
             courant.setElement(nouvelleValeur);
     }
 
+    
     public void modifieTous(Object element, Object nouvelleValeur) {
         Noeud courant = tete;
         while (courant != null) {
