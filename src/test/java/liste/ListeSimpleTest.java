ee585709298d9e9630e8bb63bd78961261af829c
MdialloC19 <mdiallo3211@gmail.com>
2026-09-17 18:24:57 +0200

    fix- supprimeTousLIsteVide test

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -120,7 +120,7 @@ public class ListeSimpleTest {
 
     @Test
     void supprimeTousListeVide() {
-        listeATester.supprimePremier(1);
+        listeATester.supprimeTous(1);
         assertNull(listeATester.tete);
         assertEquals(0, listeATester.getSize());
     }
